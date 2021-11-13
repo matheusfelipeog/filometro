30c5f331cb4c61288aabd19f14736ec3f73464f2
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-11-12 22:51:10 -0300

    Add script to request data in deolhonafila API

---
+++ filometro/models/deolhonafila.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+
+ENDPOINT = 'https://deolhonafila.prefeitura.sp.gov.br/processadores/dados.php'
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+    'Host': 'deolhonafila.prefeitura.sp.gov.br',
+    'Connection': 'keep-alive',
+    'Content-Length': '11',
+    'sec-ch-ua': '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'X-Requested-With': 'XMLHttpRequest',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'Origin': 'https://deolhonafila.prefeitura.sp.gov.br',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://deolhonafila.prefeitura.sp.gov.br/',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'pt,en-US;q=0.9,en;q=0.8,de;q=0.7',
+    'dnt': '1',
+    'sec-gpc': '1'
+}
+
+PAYLOAD = {'dados': 'dados'}
+
+
+class APIDeOlhoNaFila():
+
+    @staticmethod
+    def get_data() -> list:
+        response = requests.post(ENDPOINT, headers=HEADERS, data=PAYLOAD)
+
+        response.raise_for_status()
+
+        return response.json()
