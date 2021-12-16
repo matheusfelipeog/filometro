f3d8b5f24579d884e4cffa7096c25b64c90a1b01
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-12-16 00:04:50 -0300

    Create modality enum and implement filter by zone and modality

--- filometro/filometro.py
+++ filometro/filometro.py
@@ -20,6 +20,14 @@ class Zone(Enum):
     MEGA_DRIVES = 'MEGA-DRIVES'
 
 
+class Modality(Enum):
+    PARQUES = 'PARQUES'
+    POSTO_FIXO = 'POSTO FIXO'
+    POSTO_VOLANTE = 'POSTO VOLANTE'
+    DRIVE_THRU = 'DRIVE-THRU'
+    MEGAPOSTO = 'MEGAPOSTO'
+
+
 class Filometro():
     def __init__(self) -> None:
         self._api = APIDeOlhoNaFila()
@@ -34,3 +42,9 @@ class Filometro():
     def reload(self) -> None:
         postos = self._load_postos()
         self.postos = postos
+
+    def by_zone(self, zone: Zone) -> List[Posto]:
+        return [posto for posto in self.postos if posto.crs == zone.value]
+
+    def by_modality(self, modality: Modality) -> List[Posto]:
+        return [posto for posto in self.postos if posto.tipo_posto == modality.value]
