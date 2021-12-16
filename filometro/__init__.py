7585659468841b2d2472af852515bad9a022da35
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-12-16 15:46:22 -0300

    Define the main interface in __init__

--- filometro/__init__.py
+++ filometro/__init__.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+
+__all__ = [
+    'Zone',
+    'Modality',
+    'Situation',
+    'Immunizing',
+    'District',
+    'Filometro'
+]
+
+from filometro.enums import Zone
+from filometro.enums import Modality
+from filometro.enums import Situation
+from filometro.enums import Immunizing
+from filometro.enums import District
+
+from filometro.filometro import Filometro
