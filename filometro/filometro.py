f76d24c109ec81443eb031b450d81d3319e07cbc
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-12-16 00:59:40 -0300

    Implement Filometro.all_postos method

--- filometro/filometro.py
+++ filometro/filometro.py
@@ -17,7 +17,7 @@ class Filometro():
     def __init__(self) -> None:
         self._api = APIDeOlhoNaFila()
 
-        self.postos = self._load_postos()
+        self._postos = self._load_postos()
     
     def _load_postos(self) -> List[Posto]:
         postos_dicts = self._api.get_data()
@@ -26,13 +26,16 @@ class Filometro():
 
     def reload(self) -> None:
         postos = self._load_postos()
-        self.postos = postos
+        self._postos = postos
+
+    def all_postos(self) -> List[Posto]:
+        return self._postos
 
     def by_zone(self, zone: Zone) -> List[Posto]:
-        return [posto for posto in self.postos if posto.crs == zone.value]
+        return [posto for posto in self._postos if posto.crs == zone.value]
 
     def by_modality(self, modality: Modality) -> List[Posto]:
-        return [posto for posto in self.postos if posto.tipo_posto == modality.value]
+        return [posto for posto in self._postos if posto.tipo_posto == modality.value]
 
     def by_district(self, district: District) -> List[Posto]:
-        return [posto for posto in self.postos if posto.distrito == district.value]
+        return [posto for posto in self._postos if posto.distrito == district.value]
