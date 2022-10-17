7195692e8c6de926a798358f12a4b887d189eca6
Matheus Felipe <matheusfelipeog@protonmail.com>
2022-10-17 00:48:36 -0300

    Define the FakeAPIDeOlhoNaFila and TestFilometro classes

--- tests/test_filometro.py
+++ tests/test_filometro.py
@@ -2,9 +2,66 @@
 
 import unittest
 
+from typing import List
+
 from filometro.dataclasses import Posto
+
 from filometro.filometro import _posto_factory
 from filometro.filometro import _postos_factory
+from filometro.filometro import Filometro
+
+
+class FakeAPIDeOlhoNaFila:
+    def __init__(self) -> None:
+        self._data = [
+            {
+                'equipamento': 'test',
+                'endereco': 'test',
+                'distrito': 'Campo Limpo',
+                'crs': 'SUL',
+                'astrazeneca': '0',
+                'coronavac': '0',
+                'corona_ped': '0',
+                'pfizer': '0',
+                'pfizer_ped': '0',
+                'janssen': '0',
+                'influenza': '0',
+                'intercambialidade': '0',
+                'status_fila': 'NÃO FUNCIONANDO',
+                'tipo_posto': 'POSTO FIXO',
+                'data_hora': 'test',
+                'indice_fila': 'test',
+                'id_distrito': 'test',
+                'id_crs': 'test',
+                'id_tb_unidades': 'test',
+                'id_tipo_posto': 'test'
+            },
+            {
+                'equipamento': 'test',
+                'endereco': 'test',
+                'distrito': 'Brás',
+                'crs': 'LESTE',
+                'astrazeneca': '1',
+                'coronavac': '1',
+                'corona_ped': '1',
+                'pfizer': '1',
+                'pfizer_ped': '1',
+                'janssen': '1',
+                'influenza': '1',
+                'intercambialidade': '1',
+                'status_fila': 'FILA GRANDE',
+                'tipo_posto': 'POSTO FIXO',
+                'data_hora': 'test',
+                'indice_fila': 'test',
+                'id_distrito': 'test',
+                'id_crs': 'test',
+                'id_tb_unidades': 'test',
+                'id_tipo_posto': 'test'
+            }
+        ]
+
+    def get_data(self) -> List[dict]:
+        return self._data.copy()
 
 
 class TestPostoFactory(unittest.TestCase):
@@ -59,3 +116,10 @@ class TestPostoFactory(unittest.TestCase):
         for posto in postos:
             with self.subTest():
                 self.assertIsInstance(posto, Posto)
+
+
+class TestFilometro(unittest.TestCase):
+    """Testes relacionados a classe Filometro."""
+
+    def setUp(self):
+        self.filometro = Filometro(_api=FakeAPIDeOlhoNaFila)
