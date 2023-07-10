261324ea9bc71fc20e27ea50a6b0f5a5481aed37
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-07-10 03:44:05 -0300

    Reorder lines with +79 chars

--- filometro/deolhonafila.py
+++ filometro/deolhonafila.py
@@ -16,17 +16,23 @@ class APIDeOlhoNaFila():
     """Um wrapper da API do site 'De Olho na Fila'."""
 
     def __init__(self) -> None:
-        self.endpoint = 'https://deolhonafila.prefeitura.sp.gov.br/processadores/dados.php'
+        self.endpoint = (
+            'https://deolhonafila.prefeitura.sp.gov.br'
+            '/processadores/dados.php'
+        )
         self.payload = {'dados': 'dados'}
         self.headers = {
             'User-Agent': (
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                'Chrome/95.0.4638.69 Safari/537.36'
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                '(KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
             ),
             'Host': 'deolhonafila.prefeitura.sp.gov.br',
             'Connection': 'keep-alive',
             'Content-Length': '11',
-            'sec-ch-ua': '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
+            'sec-ch-ua': (
+                '"Google Chrome";v="95", "Chromium";'
+                'v="95", ";Not A Brand";v="99"'
+            ),
             'Accept': 'application/json, text/javascript, */*; q=0.01',
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             'X-Requested-With': 'XMLHttpRequest',
@@ -46,7 +52,11 @@ class APIDeOlhoNaFila():
     def get_data(self) -> List[dict]:
         """Pegue todos os dados do site 'De Olho na Fila'."""
 
-        response = requests.post(self.endpoint, headers=self.headers, data=self.payload)
+        response = requests.post(
+            self.endpoint,
+            headers=self.headers,
+            data=self.payload
+        )
 
         response.raise_for_status()
 
